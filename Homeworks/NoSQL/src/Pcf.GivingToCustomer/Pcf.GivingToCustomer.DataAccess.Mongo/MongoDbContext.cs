91ebca5aafc0b05274c83a7f5bab00c2e03ce290
pofoq <pofoq@mail.ru>
2025-05-01 17:06:27 +0300

    temp GivingToCustomer

---
+++ Homeworks/NoSQL/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.DataAccess.Mongo/MongoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Pcf.GivingToCustomer.DataAccess.Mongo;
+
+public class MongoDbContext
+{
+    public IMongoDatabase Database { get; }
+
+    public MongoDbContext(IOptions<MongoDbSettings> options)
+    {
+        var client = new MongoClient(options.Value.Connection);
+        Database = client.GetDatabase(options.Value.DatabaseName);
+    }
+}
