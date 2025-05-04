f274674f8b9b9828a2c91f7f7574c0cca4ab8f0e
pofoq <pofoq@mail.ru>
2025-05-04 22:31:23 +0300

    add gRPC service

---
+++ Homeworks/RabbitMQ/src/Pcf.GivingToCustomer/Pcf.GivingToCustomer.WebHost/Services/PromocodeService.cs
@@ -0,0 +1,78 @@
+﻿using Grpc.Core;
+using Pcf.GivingToCustomer.Core.Abstractions.Repositories;
+using Pcf.GivingToCustomer.Core.Domain;
+using Pcf.GivingToCustomer.WebHost.Mappers;
+using Pcf.GivingToCustomer.WebHost.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pcf.GivingToCustomer.WebHost.Services;
+
+public class PromocodeService : PromocodeCreator.PromocodeCreatorBase
+{
+    private readonly IRepository<PromoCode> _promoCodesRepository;
+    private readonly IRepository<Preference> _preferencesRepository;
+    private readonly IRepository<Customer> _customersRepository;
+
+    public PromocodeService(
+        IRepository<PromoCode> promocodeRepository, 
+        IRepository<Preference> preferencesRepository, 
+        IRepository<Customer> customersRepository)
+    {
+        _promoCodesRepository = promocodeRepository;
+        _preferencesRepository = preferencesRepository;
+        _customersRepository = customersRepository;
+    }
+
+    public override async Task<Response> Create(Request request, ServerCallContext context)
+    {
+        var errors = string.Empty;
+
+        var givePromoCodeRequest = MapToRequest(request);
+
+        if (givePromoCodeRequest.PartnerId == default ||
+            givePromoCodeRequest.PreferenceId == default ||
+            givePromoCodeRequest.PromoCodeId == default)
+        {
+            return new Response() { Result = false, Message = "Guid parse error" };
+        }
+
+        //Получаем предпочтение по имени
+        var preference = await _preferencesRepository.GetByIdAsync(givePromoCodeRequest.PreferenceId);
+
+        if (preference == null)
+        {
+            return new Response() { Result = false, Message = "BadRequest" };
+        }
+
+        //  Получаем клиентов с этим предпочтением:
+        var customers = await _customersRepository
+            .GetWhere(d => d.Preferences.Any(x =>
+                x.Preference.Id == preference.Id));
+
+        PromoCode promoCode = PromoCodeMapper.MapFromModel(givePromoCodeRequest, preference, customers);
+
+        await _promoCodesRepository.AddAsync(promoCode);
+
+        return new Response() { Result = true };
+    }
+
+    private static GivePromoCodeRequest MapToRequest(Request request)
+    {
+        _ = Guid.TryParse(request.PartnerId.Value, out Guid partnerId);
+        _ = Guid.TryParse(request.PreferenceId.Value, out Guid preferenceId);
+        _ = Guid.TryParse(request.PromoCodeId.Value, out Guid promoCodeId);
+
+        return new GivePromoCodeRequest()
+        {
+            BeginDate = request.BeginDate,
+            EndDate = request.EndDate,
+            PartnerId = partnerId,
+            PreferenceId = preferenceId,
+            PromoCode = request.PromoCode,
+            PromoCodeId = promoCodeId,
+            ServiceInfo = request.ServiceInfo,
+        };
+    }
+}
---
+++ Homeworks/RabbitMQ/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/Services/PromocodeService.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Net.Client;
+using Pcf.ReceivingFromPartner.Core.Domain;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Pcf.ReceivingFromPartner.Integration.Services;
+public class PromocodeService : IPromocodeService, IDisposable
+{
+    private readonly GrpcChannel _channel;
+    private readonly PromocodeCreator.PromocodeCreatorClient _client;
+    public PromocodeService(Uri uri)
+    {
+        _channel = GrpcChannel.ForAddress(uri);
+        _client = new PromocodeCreator.PromocodeCreatorClient(_channel);
+    }
+
+    public async Task Create(PromoCode promoCode)
+    {
+        var request = new Request()
+        {
+            PartnerId = new UUID() { Value = promoCode.Partner.Id.ToString() },
+            BeginDate = promoCode.BeginDate.ToShortDateString(),
+            EndDate = promoCode.EndDate.ToShortDateString(),
+            PreferenceId = new UUID() { Value = promoCode.PreferenceId.ToString() },
+            PromoCode = promoCode.Code,
+            ServiceInfo = promoCode.ServiceInfo,
+            PromoCodeId = new UUID() { Value = promoCode.Id.ToString() }
+        };
+
+        var result = await _client.CreateAsync(request);
+        if (result?.Result != true)
+            throw new HttpRequestException(result?.Message);
+    }
+
+    public void Dispose()
+    {
+        _channel.Dispose();
+    }
+}
